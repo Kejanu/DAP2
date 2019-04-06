8a2ed7e98d38acfc7186422b53959410610483ac
Dap21.0Easy <lavetahu@the-first.email>
2019-04-06 22:15:58 +0200

    Praktikum1

---
+++ src/LongJavaPraktikumAufg1.java
@@ -0,0 +1,18 @@
+public class LongJavaPraktikumAufg1 {
+    public int Euclid(int a, int b) {
+        if(a <0 || b < 0) {
+            throw new IllegalArgumentException("a and b must be positive");
+        }
+        if(b==0) {
+            return a;
+        }
+        else{
+            return Euclid(b,a % b);
+        }
+    }
+
+    public static void main(String[] args) {
+        LongJavaPraktikumAufg1 ljp = new LongJavaPraktikumAufg1();
+        System.out.println(ljp.Euclid(-1,0));
+    }
+}
