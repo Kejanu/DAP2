cf0b9751e7bedaf6f60a5a5fce6ca61b2462b6f0
Dap21.0Easy <lavetahu@the-first.email>
2019-04-08 00:00:36 +0200

    minor Bugfixes

--- src/Blatt1LN/SiebEratosthenes.java
+++ src/Blatt1LN/SiebEratosthenes.java
@@ -5,45 +5,48 @@ public class SiebEratosthenes {
         //test
         SiebEratosthenes test = new SiebEratosthenes();
         boolean[] b = test.siebdesEratosthenes(500);
-        for(int i = 2; i < b.length; i++) {
+        for(int i = 0; i < b.length; i++) {
             if(b[i] == true) {
                 System.out.println(i);
             }
         }
+
     }
+    //wie zum Teufel kriege ich das -o rein
     public boolean[] siebdesEratosthenes(int n) {
 
-        boolean[] b = new boolean[n];
+        boolean[] isPrime = new boolean[n];
         //0 ist keine Primzahl
-        b[0] = false;
+        isPrime[0] = false;
+        isPrime[1] = false;
         //alles auf true setzen
-        for (int i = 1; i < n; i++) {
-            b[i] = true;
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
         }
         //primzahl bestimmen
-        b = primzahl(b);
+        isPrime = primzahl(isPrime);
 
         //array zurückgeben
-        return b;
+        return isPrime;
     }
 
-    static boolean[] primzahl(boolean[]b) {
+    static boolean[] primzahl(boolean[]isPrime) {
         //laufvariable auf 0 setzen
         int i = 0;
         //Primzahlen bestimmen
-        while(i < b.length) {
+        while(i < isPrime.length) {
             int j = 2;
             //Bis zur wurzel = Laufzeit sparen yay
             while(j <= Math.sqrt(i)) {
                 //wenn i restlos teilbar durch j ist und i ungleich j ist,
                 //dann muss es sich um keine Primzahl handeln
                 if(i % j == 0 && i != j) {
-                    b[i] = false;
+                    isPrime[i] = false;
                 }
                 j++;
             }
             i++;
         }
-        return b;
+        return isPrime;
     }
 }
