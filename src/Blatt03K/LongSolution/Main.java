612e7df10e117f8c056c7534f8d54aa380b11ef9
Dap21.0Easy <lavetahu@the-first.email>
2019-04-20 04:16:13 +0200

    Fixing style of code

--- src/Blatt03K/LongSolution/Main.java
+++ src/Blatt03K/LongSolution/Main.java
@@ -1,33 +1,27 @@
 package Blatt03K.LongSolution;
+import Templates.ArrayHelper;
 import Templates.InputValidation;
 import Templates.SearchAlgorithms;
 import Templates.SortAlgorithms;
 
 public class Main {
     //warum habt ihr sie alle Main genannt
-    public static void bubbleSort(int[] array){
-        int n = array.length;
-        for(int i = 0; i<n;i++){
-            for(int j = n-1; j>i;j--){
-                if(array[j-1]>array[j]){
-                    int tmp = array[j];
-                    array[j] = array[j-1];
-                    array[j-1] = tmp;
-                }
-            }
-        }
-    }
+
 
     public static void main(String[] args) {
         long tStart, tEnd, msecs;
 
+        if(args.length  < 2){
+            System.out.println("Too many Arguments. Use Syntax: Geometrische_Suche | Binäre_Suche float\" +\n" +
+                    "\"oder Syntax: BubbleSort");
+            return;
+        }
         if(args[0].equals("BubbleSort")){
-
             int[] array = new int[50000];
-            fillArrayDescending(array);
+            ArrayHelper.fillIntArrayWithDescending(array);
             // Beginn der Messung
             tStart = System.currentTimeMillis();
-            bubbleSort(array);
+            SortAlgorithms.bubbleSort(array);
             tEnd = System.currentTimeMillis();
             msecs = tEnd - tStart;
             System.out.println("Time needed to sort 50000 Elements in msecs: "+msecs);
@@ -37,24 +31,23 @@ public class Main {
                 System.out.println("Parameter war kein Float");
                 return;
             }
-            System.out.println("");
             geometricalSearch(Float.parseFloat(args[1]),1000);
         }
         else{
-            System.out.println("Syntax: Geometrische_Suche | Binäre Suche float" +
+            System.out.println("Syntax: Geometrische_Suche | Binäre_Suche float" +
                     "oder Syntax: BubbleSort");
         }
 
-
     }
+
     //fieldSize is 1k
     public static void geometricalSearch(float time, int fieldSize){
         long tStart, tEnd, msecs;
         float convertedmsecs;
         int[] arraySearch = new int[fieldSize];
         time = time*1000;
-        fillArrayDescending(arraySearch);
 
+        ArrayHelper.fillIntArrayWithDescending(arraySearch);
         //Ermittlung vom Intervall das zu untersuchen ist
         while(true) {
             tStart = System.currentTimeMillis();
@@ -64,7 +57,7 @@ public class Main {
             convertedmsecs = msecs;
             if (convertedmsecs < time) {
                 arraySearch = new int[arraySearch.length*2];
-                fillArrayDescending(arraySearch);
+                ArrayHelper.fillIntArrayWithDescending(arraySearch);
             }
             else{
                 break;
@@ -75,6 +68,7 @@ public class Main {
 
         //binäre Suche wird gestartet
         int solution;
+
         if(arraySearch.length == 1000) {
             solution = binarySearch(time, convertedmsecs, 0, arraySearch.length);
         }
@@ -89,9 +83,11 @@ public class Main {
     //Suche nachder Länge für die gilt systemtime-toleranzgrenze <= timeInput <= systemtime +toleranzgrenze
     public static int binarySearch( float timeInput, float systemTime, int left, int right){
         float tolerantlimit = 100f;
+
         if(left == right)
             return left;
         int q = (left + right) / 2;
+
         //laufzeitmessung von bubblesort
         systemTime = binarySearchHelper(q);
         if (timeInput-tolerantlimit < systemTime && systemTime < timeInput+tolerantlimit) {
@@ -110,13 +106,10 @@ public class Main {
 
         long tStart, tEnd, msecs;
         float convertedmsecs;
+
         //length has been cutted for binarySearch purposes
         int[] arraySearchHelper = new int[arraylength];
-        fillArrayDescending(arraySearchHelper);
-
-        //System.out.println("arraylength in binarySearch time: "+arraylength);
-
-
+        ArrayHelper.fillIntArrayWithDescending(arraySearchHelper);
 
         tStart = System.currentTimeMillis();
         SortAlgorithms.bubbleSort(arraySearchHelper);
@@ -124,18 +117,9 @@ public class Main {
 
         msecs = tEnd - tStart;
         convertedmsecs = msecs;
+
         System.out.println("Bubblesort in binarySearch systemtime: " + convertedmsecs);
         System.out.println("arraylength in binarySearch time: "+arraySearchHelper.length);
         return convertedmsecs;
-    }
-
-
-
-    public static void fillArrayDescending(int[] array){
-        int counter = 0;
-        for(int i= array.length-1; i >0; i--){
-            array[counter] = i;
-            counter++;
-        }
     }
 }
