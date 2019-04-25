379b03ce1bbae35bf1f8837efa0b094ad280d4f2
Kejanu <kurznerk@gmail.com>
2019-04-25 22:35:26 +0200

    ConvexHull now working. Implemented Colors

--- src/Blatt04L/KevinSolution/ConvexHull.java
+++ src/Blatt04L/KevinSolution/ConvexHull.java
@@ -1,6 +1,7 @@
 package Blatt04L.KevinSolution;
 
 import java.sql.SQLOutput;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -10,36 +11,43 @@ import java.util.concurrent.ThreadLocalRandom;
 public class ConvexHull {
 
     public static void main(String[] args) {
-//        Point[] rndPoints = IntStream.range(0, 4)
-//                                     .mapToObj(i -> new Point(2, ThreadLocalRandom.current().nextInt(10, 100),
-//                                                                    ThreadLocalRandom.current().nextInt(10, 100)))
-//                                     .map(ConvexHull::getValidPoint)
-//                                     //.peek(System.out::println)
-//                                     .toArray(Point[]::new);
+        Point[] rndPoints = IntStream.range(0, 1001)
+         .mapToObj(i -> new Point(2, ThreadLocalRandom.current().nextDouble(10, 100),
+                                        ThreadLocalRandom.current().nextDouble(10, 100)))
+         .map(ConvexHull::getValidPoint)
+         //.peek(System.out::println)
+         .toArray(Point[]::new);
 
-        Point[] rndPoints = new Point[]{
-                new Point(2, 80, 10),
-                new Point(2, 50, 20),
-                new Point(2, 40, 30),
-                new Point(2, 30, 50),
-                new Point(2, 20, 40),
-        };
+//        Point[] rndPoints = new Point[]{
+//                new Point(2, 58, 16),
+//                new Point(2, 40, 13),
+//                new Point(2, 19, 54),
+//                new Point(2, 35, 15),
+//                new Point(2, 50, 42),
+//                new Point(2, 87, 15),
+//                new Point(2, 30, 29),
+//                new Point(2, 46, 52),
+//                new Point(2, 76, 27),
+//                new Point(2, 69, 15)
+//        };
+
+        //Arrays.stream(rndPoints).forEach(System.out::println);
 
         makePointsUnique(rndPoints);
 
         LinkedList<Point> ll = (LinkedList<Point>) simpleConvex(rndPoints);
 
-        if (ll.size() % 2 != 0) {
-            System.out.println("Something went wrong");
+        if (ll == null) {
+            System.out.println("List is null fml");
             return;
         }
 
-        Point p1;
-        Iterator it = ll.iterator();
-        p1 = (Point) it.next();
+        Point p1, p2;
+        Iterator<Point> it = ll.iterator();
+        p1 = it.next();
 
         while (it.hasNext()) {
-            System.out.println(p1 + "\tconnected to\t" + (p1 = (Point) it.next()));
+            System.out.println(p1 + "\tconnected to\t" + (p1 = it.next()));
         }
 
         new Visualization(rndPoints, ll, 1000, 1000);
@@ -81,44 +89,69 @@ public class ConvexHull {
 
     public static List<Point> simpleConvex(Point[] points) {
         // That points are in dimension = 2 has been checked before
-        LinkedList<Point> pointsList = new LinkedList<>();
+        LinkedList<Point> ll = new LinkedList<>();
 
-        for (int i = 0; i < points.length; ++i) {
+        int i = 0;
+        while (ll.isEmpty()) {
+            Point p1 = points[i];
             for (int j = 0; j < points.length; ++j) {
-
-                boolean addToList = true;
-                Point p1 = points[i];
-                Point p2 = points[j];
-
-                if (p1 == p2)
+                if (i == j)
                     continue;
-
-                for (int k = 0; k < points.length; ++k) {
-                    if (k == i || k == j)
-                        continue;
-
-                    Point p3 = points[k];
-
-                    //System.out.println("Point1: " + p1.toString() + "\t\tPoint2: " + p2.toString() + "\t\tPoint3: " + p3.toString());
-
-                    //https://math.stackexchange.com/questions/274712/calculate-on-which-side-of-a-straight-line-is-a-given-point-located/274728#274728
-                    // Line from p1=(x1,y1) to p2=(x2,y2) a point p3=(x,y)
-                    // d=(x−x1)(y2−y1)−(y−y1)(x2−x1)
-                    double position = (p3.getX() - p1.getX()) * (p2.getY() - p1.getY()) - (p3.getY() - p1.getY()) * (p2.getX() - p1.getX());
-                    if (position < 0) {
-                        // Point is on the left / wrong side of the directional line
-                        addToList = false;
-                    }
-                    //What to do, when point on the same directional line?
-
+                Point p2 = points[j];
+                if (noPointOnLeft(p1, p2, points)) {
+                    ll.add(p1);
+                    ll.add(p2);
+                    break;
                 }
-                if (addToList) {
-                    pointsList.add(p1);
-                    pointsList.add(p2);
-                }
-
             }
+            ++i;
         }
-        return pointsList;
+
+        Point last = ll.getLast();
+        int k = 0;
+        while (!ll.getFirst().equals(ll.getLast()) || ll.size() == 1) {
+            Point p = points[k];
+            if (last.equals(p)) {
+                ++k;
+                continue;
+            }
+            ++k;
+            if (noPointOnLeft(last, p, points)) {
+                ll.add(p);
+                if (ll.getLast().equals(ll.getFirst())) {
+                    return ll;
+                }
+                last = ll.getLast();
+                k = 0;
+            }
+            if (k >= points.length)
+                return null;
+        }
+        System.out.println("Last return");
+        return null;
+    }
+
+    private static boolean noPointOnLeft(Point p1, Point p2, Point[] comparePoints) {
+        //https://math.stackexchange.com/questions/274712/calculate-on-which-side-of-a-straight-line-is-a-given-point-located/274728#274728
+        // Line from p1=(x1,y1) to p2=(x2,y2) a point p3=(x,y)
+        // d=(x−x1) * (y2−y1) − (y−y1) * (x2−x1)
+        for (int i = 0; i < comparePoints.length; ++i) {
+            Point p3 = comparePoints[i];
+
+            if (p3.equals(p1) || p3.equals(p2))
+                continue;
+
+            double position = (p3.getX() - p1.getX()) *
+                              (p2.getY() - p1.getY()) -
+                              (p3.getY() - p1.getY()) *
+                              (p2.getX() - p1.getX());
+
+            if (position < 0) {
+                // Point is on the left / wrong side of the directional line
+                return false;
+            }
+            // What to do if positon = 0 bzw. when point is on the directional line
+        }
+        return true;
     }
 }
--- src/Blatt04L/KevinSolution/Visualization.java
+++ src/Blatt04L/KevinSolution/Visualization.java
@@ -7,6 +7,7 @@ import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.stream.Collectors;
 
 public class Visualization extends JFrame {
 
@@ -16,10 +17,10 @@ public class Visualization extends JFrame {
 
 
     public Visualization(Point[] points, LinkedList<Point> pointList, int width, int height) {
-        createWindow("YEET", width, height);
         this.points = points;
         this.pointList = pointList;
         this.width = width;
+        createWindow("YEET", width, height);
     }
 
     public void createWindow(String title, int width, int height) {
@@ -28,23 +29,27 @@ public class Visualization extends JFrame {
         setVisible(true);
         setSize(width, height);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBackground(new Color(37, 44, 76));
     }
 
     public void paint(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.WHITE);
 
         for (Point p : this.points) {
-            //g2.fill(new Ellipse2D.Double(p.getX() * 10, p.getY() * 10, 3, 3));
-            g2.fill(new Ellipse2D.Double(p.getX() * 10, p.getY() * 10, 6, 6));
+            // -3 cause ellipse expansion
+            g2.fill(new Ellipse2D.Double(betterX(p.getX()) - 3, betterY(p.getY()) - 3, 6, 6));
         }
 
         Point p1;
         Iterator it = pointList.iterator();
         p1 = (Point) it.next();
+        g2.setStroke(new BasicStroke(2f));
 
+        g2.setColor(new Color(0, 192, 57));
         while (it.hasNext()) {
             Point p2 = (Point) it.next();
-            Line2D line = new Line2D.Double(p1.getX() * 10, p1.getY()* 10, p2.getX()* 10, p2.getY()* 10);
+            Line2D line = new Line2D.Double(betterX(p1.getX()), betterY(p1.getY()), betterX(p2.getX()), betterY(p2.getY()));
             p1 = p2;
             g2.draw(line);
         }
