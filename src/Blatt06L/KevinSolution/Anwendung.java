0daa87414569d1a70df671d0af913dedf384243b
Kejanu <kurznerk@gmail.com>
2019-05-05 03:13:41 +0200

    Added Vorlagen

---
+++ src/Blatt06L/KevinSolution/Anwendung.java
@@ -0,0 +1,15 @@
+package Blatt06L.KevinSolution;
+
+import java.util.ArrayList;
+
+public class Anwendung {
+
+    public static ArrayList<Interval> intervalScheduling(ArrayList<Interval> intervals) {
+        // TODO: Add VolesungsAlgorithmus here
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/Blatt06L/KevinSolution/Interval.java
+++ src/Blatt06L/KevinSolution/Interval.java
@@ -1,4 +1,25 @@
 package Blatt06L.KevinSolution;
 
 public class Interval {
+
+    private int start;
+    private int end;
+
+    public Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    @Override
+    public String toString() {
+        return "Interval [ start = " + start + ", end = " + end + "]";
+    }
 }
